f9f6e7ec5dae8cc27bf73834453892a380ade55c
vkonev25-commits <vkonev25@gmail.com>
2026-06-20 00:52:19 +0700

    Last Sync: 2026-06-20 00:52 (Mobile)

---
+++ event-finder-ai/static/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'event-cache-v1';
+const urlsToCache = [
+  '/',
+  '/static/manifest.json',
+  '/static/icon-192.png',
+  '/static/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
